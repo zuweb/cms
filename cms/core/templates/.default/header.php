f8873207cb72ba708c1ae148c69dfa703a4a03b4
ZUWEB <info@zuweb.ru>
2017-01-13 13:19:53 +0400

    v0.0.9

---
+++ cms/core/templates/.default/header.php
@@ -0,0 +1,28 @@
+<?
+include($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI'].'.selection.php');
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<meta name="robots" content="<?echo $robots?>">
+	<meta name="description" content="<?echo $description?>">
+	<meta name="keywords" content="<?echo $keywords?>">
+	
+	<title><?echo $title?></title>
+	
+	<link rel="stylesheet" href="">
+
+</head>
+<body>
+
+	<nav role="navigation">
+		
+	</nav>
+
+	<main role="main">
